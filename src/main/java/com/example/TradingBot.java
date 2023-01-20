f03bde59331efe1653fd50bc31454bac877c2508
pkot-AGH <84473200+pkot-AGH@users.noreply.github.com>
2023-01-20 10:24:00 +0100

    Crrections

--- src/main/java/com/example/TradingBot.java
+++ src/main/java/com/example/TradingBot.java
@@ -3,19 +3,39 @@ package com.example;
 import com.example.model.rest.*;
 import com.example.model.security.Credentials;
 import com.example.platform.Hackathon;
+import com.example.strategy.InstrumentDestroyer;
+import com.example.strategy.MoneyCollector;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class TradingBot {
     private static final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
     private static final Logger logger = LoggerFactory.getLogger(TradingBot.class);
 
-    public static void main(String[] args) {
+    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    public static void main(String[] args){
+        logger.info("Starting the application");
+        try (final var credentialsResource = TradingBot.class.getResourceAsStream("/credentials/player2.json")) {
+            final var credentials = objectMapper.readValue(credentialsResource, Credentials.class);
+            final var marketPlugin = new Hackathon(credentials);
+
+            final var ordersController = new MoneyCollector(marketPlugin);
+
+            final var beeperHandle = scheduler.scheduleAtFixedRate(ordersController, 1, 5, SECONDS);
+        } catch (Exception exception) {
+            logger.error("Something bad happened", exception);
+        }
+    }
+    public static void main2(String[] args) {
         logger.info("Starting the application");
         try (final var credentialsResource = TradingBot.class.getResourceAsStream("/credentials/player2.json")) {
             final var credentials = objectMapper.readValue(credentialsResource, Credentials.class);
--- src/main/java/com/example/strategy/MoneyCollector.java
+++ src/main/java/com/example/strategy/MoneyCollector.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.strategy;
 
 import com.example.model.rest.*;
 import com.example.platform.Platform;
@@ -8,19 +8,21 @@ import org.slf4j.LoggerFactory;
 import java.util.Random;
 import java.util.UUID;
 
-public class OrdersController implements Runnable {
+public class MoneyCollector implements Runnable {
 
-    private static final long minQty = 10;
-    private static final long minAsk = 13;
+    private static final long minQty = 5;
+    private static final long minAsk = 15;
     private static final long minBid = 79;
 
-    private static final Logger logger = LoggerFactory.getLogger(OrdersController.class);
+    private static final int averageCount = 20;
+
+    private static final Logger logger = LoggerFactory.getLogger(MoneyCollector.class);
 
     private final Platform platform;
 
     private static final Random rg = new Random();
 
-    public OrdersController(Platform platform) {
+    public MoneyCollector(Platform platform) {
         this.platform = platform;
     }
 
@@ -38,11 +40,13 @@ public class OrdersController implements Runnable {
         }
 
         if (fetchedPortfolio instanceof PortfolioResponse.Portfolio portfolio && fetchedInstruments instanceof InstrumentsResponse.Instruments instruments) {
-            for (final var element : portfolio.portfolio()) {
-                final var instrument = element.instrument();
-                final var orders = platform.history(new HistoryRequest(instrument));
-                logger.info("instrument {} has orders {}", instrument, orders);
-            }
+            logger.info("My cash {}",portfolio.cash());
+            //for (final var element : portfolio.portfolio()) {
+            //    final var instrument = element.instrument();
+            //    final var orders = platform.history(new HistoryRequest(instrument));
+            //    logger.info("instrument {} has orders {}", instrument, orders);
+            //}
+
 
             final var selectedForBuy = instruments
                     .available()
@@ -51,23 +55,34 @@ public class OrdersController implements Runnable {
                     .toList();
 
             for (final var instrument : selectedForBuy) {
+                //logger.info("My cash {}",portfolio.cash());
                 final var history = platform.history(new HistoryRequest(instrument));
 
                 if (history instanceof HistoryResponse.History correct) {
-                    final long bid = (long) (
-                            1.1 * correct
+
+                    final long orderSum = correct
                                     .bought()
                                     .stream()
-                                    .mapToLong(b -> b.offer().price())
-                                    .average()
-                                    .orElse(minBid)
-                    );
+                                    .limit(averageCount)
+                                    .mapToLong(b->b.offer().price()*b.offer().qty())
+                                    .sum();
+                    final long orderCount = correct
+                                    .bought()
+                                    .stream()
+                                    .limit(averageCount)
+                                    .mapToLong(b->b.offer().qty())
+                                    .sum();
+                    if (orderCount == 0) continue;
+                    final long bid = orderSum/orderCount<10?minBid:(long) (0.9*orderSum/orderCount);
 
-                    final var qty = rg.nextInt((int) (portfolio.cash() / 4 / bid));
+
+                    final var qty = 1+rg.nextInt((int) (portfolio.cash() / (10 *bid)));
+
                     final var buyRequest = new SubmitOrderRequest.Buy(instrument.symbol(), UUID.randomUUID().toString(), qty, bid);
                     final var orderResponse = platform.submit(buyRequest);
 
                     logger.info("order {} placed with response {}", buyRequest, orderResponse);
+
                 }
             }
 
@@ -75,23 +90,31 @@ public class OrdersController implements Runnable {
             final var selectedElementForSell = portfolio
                     .portfolio()
                     .stream()
-                    .filter(pe -> rg.nextDouble() < 0.20)
+                    .filter(pe -> rg.nextDouble() < 0.20 && pe.qty()>0)
                     .toList();
 
             for (final var element : selectedElementForSell) {
                 final var history = platform.history(new HistoryRequest(element.instrument()));
 
                 if (history instanceof HistoryResponse.History correct) {
-                    final long ask = (long) (
-                            0.9 * correct
-                                    .bought()
-                                    .stream()
-                                    .mapToLong(b -> b.offer().price())
-                                    .average()
-                                    .orElse(minAsk)
-                    );
+
+                    final long orderSum = correct
+                            .bought()
+                            .stream()
+                            .limit(averageCount)
+                            .mapToLong(b->b.offer().price()*b.offer().qty())
+                            .sum();
+                    final long orderCount = correct
+                            .bought()
+                            .stream()
+                            .limit(averageCount)
+                            .mapToLong(b->b.offer().qty())
+                            .sum();
+                    if (orderCount == 0) continue;
+                    final long ask = 1.1*orderSum/orderCount<minAsk ? minAsk:(long) (1.1*orderSum/orderCount);
 
                     final long qty = Math.min(element.qty(), minQty);
+
                     final var sellRequest = new SubmitOrderRequest.Sell(element.instrument().symbol(), UUID.randomUUID().toString(), qty, ask);
                     final var orderResponse = platform.submit(sellRequest);
 
